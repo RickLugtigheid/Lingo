729383886681fc4a3d925593e76d6f7811daae40
Rick <rlugtigheid77@gmail.com>
2020-01-10 14:21:31 +0100

    mastermind done
    mastermind is afgemaakt, nu nog een paar keer testen om te debuggen.

--- masterMind/assets/js/mastermindMotor.js
+++ masterMind/assets/js/mastermindMotor.js
@@ -2,6 +2,7 @@ const colorPens = ["red", "green", "blue", "yellow", "brown","orange","black","w
 
 var curRow = 8;
 var grid = document.getElementById("gridContainer");
+var pens = document.getElementById("pens");
 
 startGame(4, false);
 function startGame(rows, useSameColor){
@@ -12,7 +13,6 @@ function startGame(rows, useSameColor){
     while(code.length < rows){
         gridRows.push(" 50px");
         var check = colorPens[Math.floor(Math.random() * colorPens.length)]
-        console.log(check);
         if(code.includes(check) && useSameColor == false){
            console.log("Can't have 2 colors in code!");
         }else{
@@ -44,31 +44,33 @@ function checkRow(){
         // make sure you can only interact with the curRow
         if(curRow > 0){
             for(var n = 1; n < 5; n++){
-                console.log(grid.children[(curRow * 4) - n])
                 grid.children[(curRow * 4) - n].id = "pos" + n;
                 grid.children[(curRow * 4) - n].firstChild.setAttribute("onClick", "setColor(this)");
                 checkOrMake = false;
             }
         }else{
-            alert("you lose: no more rows left!")
+            alert("you lose: no more rows left!");
+            reset();
         }
     }else{
-        var whitePen = 0;
-        var redPen = 0;
+        var curPen = 0;
+        var redPens = 0;
         for(var r = 1; r < 5; r++){
             var cur = grid.children[(curRow * 4) - r].firstChild;
             cur.classList.remove("itemRound");
             var curString = cur.classList.toString();
-            console.log(curString);
             cur.classList.add("itemRound");
 
             // check if we need a red pen
             if(curString == code[(r -1)]){
-                //doesn't work correctly
-                whitePen++;
-            }else if(code.includes(curString)){
+                pens.children[(curRow-1)].children[curPen].classList += ' red';
+                curPen++;
+                redPens++;
+
                 // check if we need a white pen
-                redPen++;
+            }else if(code.includes(curString)){
+                pens.children[(curRow-1)].children[curPen].classList += ' white';
+                curPen++;
             }
             // make sure you can't interact with the buttons anymore
             cur.setAttribute("onClick", "");
@@ -76,8 +78,11 @@ function checkRow(){
         // sets the next rows to use
         checkOrMake = true;
         curRow--;
-        alert(whitePen+ " White pens, " + redPen + "Red pens");
-        if(redPen == 4){ alert("You win!")}
+        //you won, restart the game
+        if(redPens >= 4){ 
+            alert("You win!");
+            reset();
+        }
         // set the next row interactable
         checkRow();
     }
@@ -94,5 +99,18 @@ function colorSelect(color){
 function setColor(element){
     if(selectedColor != null){
         element.classList = "itemRound " + selectedColor;
+    }
+}
+
+function reset(){
+    var restart = prompt("play again?\n type 'no' to exit\n type anyting to restart")
+    if(restart != "no" && restart != "No" && restart != "NO"){
+        grid.innerHTML = "";
+        grid.style = '';
+        startGame(4, false);
+        for(var p = 1; p < 10; p++){
+            for(var c = 0; c < 4; c++)
+            pens.children[(p-1)].children[c].classList = 'helpPens';
+        }
     }
 }
